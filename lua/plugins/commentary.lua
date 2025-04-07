f4e557973eae02e9038347f892e663a40d08c188
Skar3Krow <dregoexistenciodotofficial@gmail.com>
2025-04-07 20:41:16 +0530

    Added multi-line comment funcitonality

--- lua/plugins/commentary.lua
+++ lua/plugins/commentary.lua
@@ -1,6 +1,12 @@
 return {
   "shoukoo/commentary.nvim",
   config = function()
-    require("commentary").setup()
+    require("commentary").setup({
+      languages = {
+        go = { single_line_comment = "//", multiline_comment = { "/*", "*/" }, prefer_multiline = true },
+        typescript = { single_line_comment = "//", multiline_comment = { "/**", "*/" }, prefer_mutiline = true },
+        rust = { single_line_comment = "//", multiline_comment = { "/*", "*/" }, prefer_multiline = true },
+      },
+    })
   end,
 }
