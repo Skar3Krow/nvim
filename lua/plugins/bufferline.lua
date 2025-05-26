508d077437204ed0cca1463627e406c3f0fa6428
Skar3Krow <dregoexistenciodotofficial@gmail.com>
2025-05-26 19:46:46 +0530

    Added diagnostics to buffer

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -16,6 +16,33 @@ return {
             separator = true, -- use a "true" to enable the default, or set your own character
           },
         },
+        custom_areas = {
+          right = function()
+            local result = {}
+            local seve = vim.diagnostic.severity
+            local error = #vim.diagnostic.get(0, { severity = seve.ERROR })
+            local warning = #vim.diagnostic.get(0, { severity = seve.WARN })
+            local info = #vim.diagnostic.get(0, { severity = seve.INFO })
+            local hint = #vim.diagnostic.get(0, { severity = seve.HINT })
+
+            if error ~= 0 then
+              table.insert(result, { text = "  " .. error, link = "DiagnosticError" })
+            end
+
+            if warning ~= 0 then
+              table.insert(result, { text = "  " .. warning, link = "DiagnosticWarn" })
+            end
+
+            if hint ~= 0 then
+              table.insert(result, { text = "  " .. hint, link = "DiagnosticHint" })
+            end
+
+            if info ~= 0 then
+              table.insert(result, { text = "  " .. info, link = "DiagnosticInfo" })
+            end
+            return result
+          end,
+        },
       },
       vim.keymap.set("n", "<leader>bd", ":bd<CR>", { desc = "Delete buffer" }),
       vim.keymap.set("n", "<Tab>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next buffer" }),
